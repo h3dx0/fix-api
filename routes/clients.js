dc7a85982497c22be9f3c1b226c52cc2f0d65104
h3dx0 <rob3rto@openmailbox.org>
2019-08-15 22:22:17 -0500

    add client endpoint

--- routes/clients.js
+++ routes/clients.js
@@ -7,4 +7,22 @@ router.get('/', async function(req, res, next) {
 	res.json(clients);
 });
 
+router.post('/add', async function(req,res,next){
+	const body = req.body;
+	const client = {
+		name: body.name,
+		last_name: body.last_name,
+		email: body.email,
+		mobil_phone: body.mobile,
+		password: body.password,//TODO encriptar esto
+		photo: body.photo
+	}
+	try {
+		await db('clients').insert(client)		
+		res.status(200).json({rc: 0, msg: "Saved!!"})
+	} catch(e) {
+		res.status(500).json({rc: -1, msg: e})
+		console.log(e);
+	}
+})
 module.exports = router;
